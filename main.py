7d738ae782fa0f7ee58bcc48ed70941b614ac3e0
liangqin.fan <liangqin.fan@gmail.com>
2023-10-27 14:20:00 +0800

    add bids

--- main.py
+++ main.py
@@ -24,23 +24,31 @@ def sell_order_quote(token_in, token_out, fee, price_decrease):
     quoter = get_quoter()
     # 第一步, 拿到当前的报价
     curr_price = quoter.caller.quoteExactInputSingle(token_in, token_out, fee, one_eth, 0)
-    print(f"current price: {curr_price}")
+    # print(f"current price: {curr_price}")
 
     # 第二步, 设置预期的价格
     # 因为我们是要卖出ETH. 池子中ETH会增加, 意味着价格会下降, 所以目标卖出价要比当前价格低.
-    target_sell_price = algo.price_to_sqrtp(curr_price - price_decrease)  # sell eth make price decrease 300U
+    target_price = curr_price - price_decrease
+    target_sell_price = algo.price_to_sqrtp(target_price)  # sell eth make price decrease 300U
 
     # 第三步, 使用一个足够大的input, 将目标价格以上的流动性全部买光, 那么计算得到的结果将是最大的可用流动性.
     max_input = 100000000 * one_eth
     max_output = quoter.caller.quoteExactInputSingle(token_in, token_out, fee, max_input, target_sell_price)
-    print(f"max liquidity for output: {max_output}")
+    # print(f"max liquidity for output: {max_output}")
 
     # 第四步, 使用max_output, 反向预估
     amount_in = quoter.caller.quoteExactOutputSingle(token_in, token_out, fee, max_output, target_sell_price)
-    print(f"estimate buy-able liquidity: {amount_in/one_eth}")
+    # print(f"estimate buy-able liquidity: {amount_in/one_eth}")
+    return target_price, amount_in, max_output
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    sell_order_quote(WETH_TOKEN, USDC_TOKEN, FeeAmount_Medium, 100*one_usdc)
+    totalLiqudity = 0
+    for i in range(10):
+        p, output, _ = sell_order_quote(WETH_TOKEN, USDC_TOKEN, FeeAmount_Medium, 50 * i * one_usdc)
+
+        print(f"price: {p/one_usdc}, liquidity: {(output)/one_eth}")
+        totalLiqudity += output
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
